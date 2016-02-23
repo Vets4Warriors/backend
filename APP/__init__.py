032c23f02328db11d2679873ca9b1f3827c50a28
austin <austin.cawley@gmail.com>
2016-02-23 17:11:38 -0500

    Got rid of mongorest. Ugh.

--- APP/__init__.py
+++ APP/__init__.py
@@ -3,11 +3,7 @@
 """
 from flask import Flask
 from flask_mongoengine import MongoEngine
-from flask_mongorest import MongoRest
 from flask_stathat import StatHat
-
-from flask_mongorest.views import ResourceView
-from flask_mongorest import methods
 
 app = Flask(__name__)
 
@@ -20,17 +16,9 @@ app.config['MONGODB_SETTINGS'] = {
 app.config['STATHAT_EZ_KEY'] = 'o3OaE05mySW3g9RH'
 
 db = MongoEngine(app)
-api = MongoRest(app)
 stathat = StatHat(app)
 
-from APP import resources as res
-
-
-@api.register(name="locations", url='/locations/')
-class LocationView(ResourceView):
-    resource = res.LocationResource
-    methods = [methods.Create, methods.Update, methods.Delete, methods.Fetch, methods.List]
-
+from APP import routes
 
 if __name__ == '__main__':
     app.run(port=5000)
--- APP/routes.py
+++ APP/routes.py
@@ -2,24 +2,31 @@
 
 """
 from flask import jsonify, make_response
-import logging
 from APP import app, db, stathat
 
 
 @app.route('/')
 def index():
     stathat.count('index_hits', 1)
-    return make_response(jsonify("This is Vets 4 Warriors!"), 200)
+    return make_response(jsonify(response="This is Vets 4 Warriors!"), 200)
+
+
+# Add a resource
+@app.route('/locations', methods=['POST'])
+def add_location():
+    return make_response(jsonify(response="All good"), 200)
+
+#
 
 
 @app.errorhandler(404)
 def not_found(error):
     stathat.count('404_hits', 1)
-    return make_response(jsonify("Resource does not exist"), 404)
+    return make_response(jsonify(response="Resource does not exist"), 404)
 
 
 @app.errorhandler(500)
 def server_error(error):
-    stathat.count('500)hits', 1)
-    return make_response(jsonify("uh oh server error"), 500)
+    stathat.count('500_hits', 1)
+    return make_response(jsonify(response="uh oh server error"), 500)
 
