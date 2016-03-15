c7bf14e2994dfb9f685f6bfc0495f1547412d25d
Austin Cawley-Edwards <austin.cawley@gmail.com>
2016-03-15 03:27:23 -0400

    Updated the required fields

--- app/documents/Location.py
+++ app/documents/Location.py
@@ -32,8 +32,7 @@ class LocationModel:
         'addedOn': sg.fields.DateTime
     }
 
-    required = ['name', 'address', 'website', 'phone', 'email', 'locationType', 'coverages', 'services', 'tags',
-                'comments', 'addedBy']
+    required = ['name', 'website', 'locationType', 'coverages', 'services', 'addedBy']
 
 
 class Location(db.Document):
@@ -45,23 +44,24 @@ class Location(db.Document):
     #     'queryset_class': LocQuery
     # }
 
-    name = db.StringField(max_length=255, unique=True)
+    name = db.StringField(max_length=255, unique=True, required=True)
     address = db.EmbeddedDocumentField(Address)
     hqAddress = db.EmbeddedDocumentField(Address)
-    website = db.URLField()
+    website = db.URLField(required=True)
     phone = db.StringField(max_length=15)
     email = db.EmailField()
     ratings = db.EmbeddedDocumentListField(Rating, default=[])
-    locationType = db.StringField(max_length=255)
-    coverages = db.ListField(db.StringField(choices=['International', 'National', 'Regional', 'State', 'Local', '']))
+    locationType = db.StringField(max_length=255, required=True)
+    coverages = db.ListField(db.StringField(choices=['International', 'National', 'Regional', 'State', 'Local', ''],
+                                            required=True))
     # Will probably also want to limit the choices here eventually?
-    services = db.ListField(db.StringField(max_length=255))
+    services = db.ListField(db.StringField(max_length=255, required=True))
 
-    tags = db.ListField(db.StringField(max_length=255))
+    tags = db.ListField(db.StringField(max_length=255), default=[])
     # Eventually we can make this a threaded list like a forum
     comments = db.StringField()
 
-    addedBy = db.StringField()
+    addedBy = db.StringField(required=True)
     addedOn = db.DateTimeField(default=datetime.utcnow)
 
     def to_json(self):
@@ -92,16 +92,32 @@ class Location(db.Document):
         """
         # Could get more intense about validation/formatting data
         # Will see if the users mess it up enough / harder on the front end
-        address = Address.from_data(data['address'], validate=validate)
 
-        # Not required
+        phone = None
+        if 'phone' in data and type(data['phone']) is str:
+            phone = data['phone']
+
+        email = None
+        if 'email' in data and type(data['email']) is str:
+            email = data['email']
+
+        comments = None
+        if 'comments' in data and type(data['comments']) is str:
+            comments = data['comments']
+
+        address = None
+        if 'address' in data and data['address'] is not None:
+            address = Address.from_data(data['address'], validate=validate)
+
         hqAddress = None
         if 'hqAddress' in data and data['hqAddress'] is not None:
             hqAddress = Address.from_data(data['hqAddress'], validate=validate)
 
         # Process all the tags to be lowercase but services to be capitalized
-        for i in range(0, len(data['tags'])):
-            data['tags'][i] = data['tags'][i].lower()
+        tags = None
+        if 'tags' in data and type(data['tags']) is list:
+            for i in range(0, len(data['tags'])):
+                data['tags'][i] = data['tags'][i].lower()
 
         for i in range(0, len(data['services'])):
             data['services'][i] = data['services'][i].capitalize()
@@ -111,13 +127,13 @@ class Location(db.Document):
             address=address,
             hqAddress=hqAddress,
             website=data['website'],
-            phone=data['phone'],
-            email=data['email'],
+            phone=phone,
+            email=email,
             locationType=data['locationType'],
             coverages=data['coverages'],
             services=data['services'],
-            tags=data['tags'],
-            comments=data['comments'],
+            tags=tags,
+            comments=comments,
             addedBy=data['addedBy']
         )
         if validate:
